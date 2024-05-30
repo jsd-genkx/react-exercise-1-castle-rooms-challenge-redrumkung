40b7db7e80dffba3097cafc90833b5a0f119341f
redrumkumg <n.ronnapat@github.com>
2024-05-30 13:05:44 +0700

    fixned Room name

--- src/components/02_Tower.jsx
+++ src/components/02_Tower.jsx
@@ -3,7 +3,7 @@ import Chamber from "./03_Chamber";
 const Tower = ({ message1, onReply }) => {
   return (
     <>
-      <div>Castle</div>
+      <div>Tower</div>
       <span>Message for JSD7: {message1}</span>
       <Chamber message1={message1} onReply={onReply} />
     </>
--- src/components/04_Room.jsx
+++ src/components/04_Room.jsx
@@ -3,7 +3,7 @@ import Hall from "./05_Hall"
 const Room = ({ message1, onReply }) => {
   return (
     <>
-      <div>Hall</div>
+      <div>Room</div>
       <span>Message for JSD7: {message1}</span>
       <Hall message1={message1} onReply={onReply} />
     </>
